5d4dc6b23398c34a36c0a93c77aefbe74cff1c8a
munabeel <munabeel@ABSHER-DEV-NPE.elm.com.sa>
2023-12-06 09:46:13 +0300

    initial: Initial commit

---
+++ container-app/src/main/java/com/mna/container/app/ContainerAppApplication.java
@@ -0,0 +1,13 @@
+package com.mna.container.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContainerAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ContainerAppApplication.class, args);
+	}
+
+}
